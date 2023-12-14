ea99d60ef215e190347eb25afa36b1af1f2e0f94
hyunw3 <0176778602@naver.com>
2023-12-14 08:08:17 +0000

    chore: add experiment scripts 10-1 for DKD, MiB, PLOP

--- z_exp_cmd/1212_PLOP_and_MiB.sh
+++ z_exp_cmd/1212_PLOP_and_MiB.sh
@@ -10,6 +10,14 @@ cd ../z_exp_individual_cmd
 
 # ./train_voc_15-1_PLOP_4GPU.sh
 # after revision (remove bg classifier detach)
-./train_voc_15-1_MiB_onlyIncremental.sh 
-./train_voc_15-1_MiB.sh 
+# ./train_voc_15-1_MiB_onlyIncremental.sh 
+# ./train_voc_15-1_MiB.sh 
 
+# 10-1
+./train_voc_10-1_MiB.sh
+wait_exec 90787 echo ""
+wait_exec 60621 echo ""
+wait_exec 61618 echo ""
+alert_knock echo "all task is done"
+./train_voc_10-1_PLOP.sh
+./train_voc_10-1_DKD.sh
